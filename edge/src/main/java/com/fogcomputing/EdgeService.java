60651354d53a63c6b03fa1093489cd21bf32fd29
Jonas Arnhold <jonasarnhold@web.de>
2024-05-31 21:42:20 +0200

    Implement sending mechanism draft and dead letter handling

--- edge/src/main/java/com/fogcomputing/EdgeService.java
+++ edge/src/main/java/com/fogcomputing/EdgeService.java
@@ -4,20 +4,20 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.zeromq.ZMQ;
+
 public class EdgeService {
 
 	public static void main(String[] args) {
 
-		// assuming we need 3 threads in total
-		ExecutorService executor = Executors.newFixedThreadPool(3);
-
-		// using String and not a custom object for a first draft
+		ExecutorService executor = Executors.newFixedThreadPool(3); // assuming we need 3 threads in total
 		ConcurrentLinkedQueue<SensorData> messageBuffer = new ConcurrentLinkedQueue<>();
 
 		SensorDataCollector sensorDataCollector = new SensorDataCollector(messageBuffer);
 		executor.submit(sensorDataCollector);
 
-		MessageSender messageSender = new MessageSender(messageBuffer);
+		ZMQ.Socket cloudSocket = null; // just a dummy for now
+		MessageSender messageSender = new MessageSender(messageBuffer, cloudSocket);
 		executor.submit(messageSender);
 	}
 
--- edge/src/main/java/com/fogcomputing/MessageSender.java
+++ edge/src/main/java/com/fogcomputing/MessageSender.java
@@ -1,26 +1,38 @@
 package com.fogcomputing;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
-import lombok.RequiredArgsConstructor;
+import org.zeromq.ZMQ;
 
-@RequiredArgsConstructor
 public class MessageSender implements Runnable {
 
 	private static final int DEFAULT_BATCH_SIZE = 15; // we send every 15 seconds, sensor data is written/collected every second
 
 	private final ConcurrentLinkedQueue<SensorData> messageBuffer;
+	private final ZMQ.Socket cloudSocket;
+	private final List<SensorDataBatch> deadLetterBuffer;
+
+	public MessageSender(ConcurrentLinkedQueue<SensorData> messageBuffer, ZMQ.Socket cloudSocket) {
+		this.messageBuffer = messageBuffer;
+		this.cloudSocket = cloudSocket;
+		this.deadLetterBuffer = new ArrayList<>();
+	}
 
 	@Override
 	public void run() {
 		while (true) {
 
-			List<SensorData> batch = new ArrayList<>(DEFAULT_BATCH_SIZE);
+			if (!deadLetterBuffer.isEmpty()) {
+				deadLetterBuffer.stream().filter(this::trySendToCloud).forEach(deadLetterBuffer::remove);
+			}
 
+			List<SensorData> batch = new ArrayList<>(DEFAULT_BATCH_SIZE);
 			while (messageBuffer.peek() != null) {
 				SensorData sensorData = messageBuffer.poll();
 				batch.add(sensorData);
@@ -29,9 +41,24 @@ public class MessageSender implements Runnable {
 			Timestamp currentDateTime = new Timestamp(System.currentTimeMillis());
 			SensorDataBatch sensorDataBatch = new SensorDataBatch(batch, currentDateTime);
 			System.out.printf("Batch ready to sent: %s\n", sensorDataBatch);
-			// TODO send sensorDataBatch to Cloud service
+//			trySendToCloud(sensorDataBatch); // not tested yet
 
 			ThreadUtils.sleep(15, TimeUnit.SECONDS);
+		}
+	}
+
+	private boolean trySendToCloud(SensorDataBatch sensorDataBatch) {
+		try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			 ObjectOutputStream oos = new ObjectOutputStream(bos))
+		{
+			oos.writeObject(sensorDataBatch);
+			cloudSocket.send(bos.toByteArray());
+			return true;
+		}
+		catch (Exception e) {
+			System.err.println("Error sending message: " + e.getMessage());
+			deadLetterBuffer.add(sensorDataBatch);
+			return false;
 		}
 	}
 
