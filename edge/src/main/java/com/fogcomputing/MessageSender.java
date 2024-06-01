cea3f612cb2acd524ccec6a2b4e6a41b8e150564
Jonas Arnhold <jonasarnhold@web.de>
2024-06-01 22:43:14 +0200

    Handle failure in MessageSender

--- edge/src/main/java/com/fogcomputing/MessageSender.java
+++ edge/src/main/java/com/fogcomputing/MessageSender.java
@@ -9,16 +9,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
-import org.zeromq.ZMQException;
 
 public class MessageSender implements Runnable {
 
 	private static final int DEFAULT_BATCH_SIZE = 15; // we send every 15 seconds, sensor data is written/collected every second
 	private static final int POLLING_TIMEOUT = 1000;
-	private static final int MAX_TRIES = 3;
 
 	private final ConcurrentLinkedQueue<SensorData> messageBuffer;
-	private final List<SensorDataBatch> deadLetterBuffer;
 	private final ZMQ.Socket cloudSocket;
 	private final ZMQ.Poller poller;
 
@@ -27,7 +24,6 @@ public class MessageSender implements Runnable {
 			throw new IllegalArgumentException("Invalid socket type: %s. Expecting SocketType.REQ".formatted(cloudSocket.getSocketType()));
 		}
 		this.messageBuffer = messageBuffer;
-		this.deadLetterBuffer = new ArrayList<>();
 		this.cloudSocket = cloudSocket;
 		this.poller = ZContextProvider.getInstance().createPoller(1);
 		this.poller.register(cloudSocket);
@@ -36,10 +32,6 @@ public class MessageSender implements Runnable {
 	@Override
 	public void run() {
 		while (!Thread.currentThread().isInterrupted()) {
-
-			if (!deadLetterBuffer.isEmpty()) {
-				deadLetterBuffer.stream().filter(this::trySendToCloud).forEach(deadLetterBuffer::remove);
-			}
 
 			List<SensorData> batch = new ArrayList<>(DEFAULT_BATCH_SIZE);
 			while (messageBuffer.peek() != null) {
@@ -55,35 +47,26 @@ public class MessageSender implements Runnable {
 		}
 	}
 
-	private boolean trySendToCloud(SensorDataBatch sensorDataBatch) {
-		System.out.printf("Trying to sent batch: %s\n", sensorDataBatch);
+	private void trySendToCloud(SensorDataBatch sensorDataBatch) {
 		try {
-			int tries = 0;
-			while (tries < MAX_TRIES) {
-				tries++;
-				cloudSocket.send(sensorDataBatch.serialize());
+			System.out.printf("Trying to sent batch: %s\n", sensorDataBatch);
+			cloudSocket.send(sensorDataBatch.serialize());
+			while (!Thread.currentThread().isInterrupted()) {
 				poller.poll(POLLING_TIMEOUT);
 				if (poller.pollin(0)) {
 					String response = cloudSocket.recvStr();
 					System.out.println("Got response from cloud server: " + response);
-					return true;
+					return;
 				}
 			}
-			return handleFailedSend(sensorDataBatch);
 		}
 		catch (IOException e) {
 			System.err.printf("Error sending message: %s. Possibly due to a serialization error?%n", e.getMessage());
-			return handleFailedSend(sensorDataBatch);
+			System.exit(1);
 		}
-		catch (ZMQException e) {
-			System.err.printf("Error sending message: %s. Probably server did not respond within %d tries...", e.getMessage(), MAX_TRIES);
-			return handleFailedSend(sensorDataBatch);
+		catch (Exception e) {
+			System.err.printf("Unknown error sending message: %s.", e.getMessage());
 		}
-	}
-
-	private boolean handleFailedSend(SensorDataBatch sensorDataBatch) {
-		deadLetterBuffer.add(sensorDataBatch);
-		return false;
 	}
 
 }
